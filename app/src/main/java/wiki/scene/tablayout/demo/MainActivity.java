5d22e17dffeb0b39fa027a1356bdfb5069a49360
任俊家 <renjunjia1@163.com>
2020-05-08 10:36:23 +0800

    Initial commit

---
+++ app/src/main/java/wiki/scene/tablayout/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package wiki.scene.tablayout.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
